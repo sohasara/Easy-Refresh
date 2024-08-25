fbbd55dacb4387c41356e45a09d7942096c80f19
Nadia Islam <sohasara853@gmail.com>
2024-08-25 21:44:54 +0600

    add dependancy

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: true),
-      home: Scaffold(),
+      home: const Scaffold(),
     );
   }
 }
