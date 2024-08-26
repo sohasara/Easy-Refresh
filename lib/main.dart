029e0c531409a7f8358f6e62fb27daab46edcf3e
Nadia Islam <sohasara853@gmail.com>
2024-08-26 11:37:24 +0600

    modify

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:easy_refresh/easy_refresh.dart';
+import 'package:easy_refresh_flutter/simple_example.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -14,49 +14,6 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       theme: ThemeData.light(useMaterial3: true),
       home: const SimpleEasyRefresh(),
-    );
-  }
-}
-
-class SimpleEasyRefresh extends StatefulWidget {
-  const SimpleEasyRefresh({super.key});
-
-  @override
-  State<SimpleEasyRefresh> createState() => _SimpleEasyRefreshState();
-}
-
-class _SimpleEasyRefreshState extends State<SimpleEasyRefresh> {
-  List<int> _items = List.generate(20, (index) => index);
-  int _nextItem = 20;
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('EasyRefresh Example'),
-      ),
-      body: EasyRefresh(
-        onRefresh: () async {
-          await Future.delayed(const Duration(seconds: 2));
-          setState(() {
-            _items = List.generate(20, (index) => index);
-            _nextItem = 20;
-          });
-        },
-        onLoad: () async {
-          await Future.delayed(const Duration(seconds: 2));
-          setState(() {
-            _items.addAll(List.generate(10, (index) => _nextItem++));
-          });
-        },
-        child: ListView.builder(
-          itemCount: _items.length,
-          itemBuilder: (context, index) {
-            return ListTile(
-              title: Text('Item ${_items[index]}'),
-            );
-          },
-        ),
-      ),
     );
   }
 }
